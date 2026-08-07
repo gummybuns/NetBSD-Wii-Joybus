f14bda643c70766f1325672b2d97bd87cdb25dcb
zac <gummybuns@protonmail.com>
2026-08-07 15:36:19 -0400

    got something to work gba->wii

--- puffboy_advance/shared/command.h
+++ puffboy_advance/shared/command.h
@@ -4,15 +4,21 @@
 #define CMD_NTH_ENTRY 0x0001
 #define WORD_CNT(n) ((sizeof(n)+3)/4)
 
+struct packet {
+	uint8_t seq;
+	uint8_t cmd;
+	uint16_t data;
+};
+
 struct nth_entry_request {
 	uint32_t	parent_fileid;
 	uint32_t	n;
 };
 
 struct nth_entry_response {
-	unsigned char	exists;
+	char		exists;
+	char		va_type;
 	uint32_t 	va_fileid;
-	uint8_t		va_type;
-	char 		name[32];
+	char 		name[31];
 };
 #endif
